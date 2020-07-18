ab537237839e0f69dde2f9f4b82a0eaa577e76fb
Pat Turner <patrick.turner@nhs.net>
2020-07-18 13:17:46 +0100

    sync vscode extensions

--- vscode/vscode-extensions.sh
+++ vscode/vscode-extensions.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
-# Visual Studio Code :: Package list
-pkglist=(
-christian-kohler.path-intellisense
-davidanson.vscode-markdownlint
-EditorConfig.EditorConfig
-foxundermoon.shell-format
-mikestead.dotenv
-ms-python.python
-ms-python.vscode-pylance
-ms-azuretools.vscode-docker
-njpwerner.autodocstring
-oderwat.indent-rainbow
-shd101wyy.markdown-preview-enhanced
-shuworks.vscode-table-formatter
-stevejpurves.cucumber
-yzane.markdown-pdf
-)
+# store in this directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# install extension list
+pkglist=(`cat $DIR/vscode-extensions.txt`)
 for i in ${pkglist[@]}; do
   code --install-extension $i
 done
+
+# update extension list
+code --list-extensions > $DIR/vscode-extensions.txt
