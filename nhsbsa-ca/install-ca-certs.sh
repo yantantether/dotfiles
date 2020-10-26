55117d505e8155456c56b82bc2cbf85d89a2cea9
Pat Turner <patrick.turner@nhs.net>
2020-10-26 20:51:43 +0000

    better cacert script

--- nhsbsa-ca/install-ca-certs.sh
+++ nhsbsa-ca/install-ca-certs.sh
@@ -1,20 +1,66 @@
 #!/bin/bash
 
-# download ca and import as trusted
-if test -f ~/Downloads/nhsbsa_aws_ca_2019-2029.pem; then
-  wget -P ~/Downloads https://bsa2468.atlassian.net/wiki/download/attachments/577994812/nhsbsa_aws_ca_2019-2029.pem
-fi
-sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ~/Downloads/nhsbsa_aws_ca_2019-2029.pem
+JAVA_HOME=${JAVA_HOME:-`/usr/libexec/java_home`}
+SYSTEM_KEYCHAIN="/Library/Keychains/System.keychain"
+KS_PASS=changeit
 
-# download intermediate and import
-if test -f ~/Downloads/nhsbsa_aws_int_ca_2019-2024.pem; then
-  wget -P ~/Downloads https://bsa2468.atlassian.net/wiki/download/attachments/577994812/nhsbsa_aws_int_ca_2019-2024.pem
-fi
-sudo security import ~/Downloads/nhsbsa_aws_int_ca_2019-2024.pem -k /Library/Keychains/System.keychain
+#find cacerts files
+function find_cacerts {
+  if [ ! -f ~/.cacerts ]; then
+    echo "finding cacerts to update..."
+    find / -name cacerts -print 2>/dev/null | grep -v System > ~/.cacerts
+    echo
+  fi
+  echo "~/.cacerts file contains these files:"
+  echo
+  cat ~/.cacerts
+  echo
+  echo "Delete ~/.cacerts and rerun script to rescan."
+}
 
-java_home_path=`/usr/libexec/java_home`
-sudo keytool -delete -alias nhsbsa.aws.root.ca.cert.pem -keystore "$java_home_path/lib/security/cacerts"
-sudo keytool -delete -alias nhsbsa.aws.int.ca.cert.pem -keystore "$java_home_path/lib/security/cacerts"
-sudo keytool -importcert -file ~/Downloads/nhsbsa_aws_ca_2019-2029.pem -keystore "$java_home_path/lib/security/cacerts" -alias nhsbsa.aws.root.ca.cert.pem
-sudo keytool -importcert -file ~/Downloads/nhsbsa_aws_int_ca_2019-2024.pem -keystore "$java_home_path/lib/security/cacerts" -alias nhsbsa.aws.int.ca.cert.pem
+#add trusted cert for root cert
+function add_trusted_cert_os {
+  sudo security add-trusted-cert -d -r trustRoot -k $SYSTEM_KEYCHAIN "$1"
+}
 
+#import cert for intermediate cert
+function import_cert_os {
+  sudo security import "$1" -k /Library/Keychains/System.keychain
+}
+
+#install cert in JRE
+function install_cert_jre {
+  cat ~/.cacerts | while read cacert
+  do
+    cert_name=$(basename $1)
+    echo "deleting alias $cert_name in $cacert:"
+    keytool -delete -alias $cert_name -storepass changeit -keystore "$cacert"
+    echo
+    echo "adding cert alias $cert_name from $1 in $cacert"
+    keytool -import -alias $cert_name -file $1 -noprompt -trustcacerts -storepass changeit -keystore "$cacert"
+  done
+}
+
+#find cacerts in this system
+find_cacerts
+
+echo
+echo "These certificates found in ~/Downloads folder:"
+echo
+ls ~/Downloads/*.pem
+echo
+
+echo "Enter path to root certificate:"
+read root_cert
+
+add_trusted_cert_os $root_cert
+install_cert_jre $root_cert
+
+echo "Enter path to intermediate certificate:"
+read int_cert
+
+import_cert_os $int_cert
+install_cert_jre $int_cert
+
+echo
+echo cacerts imported
